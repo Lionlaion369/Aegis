8aaea2601a05ce29a2e31b159cf90e2d775b3917
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2025-12-09 12:27:16 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const Aegis = require('./core/aegis_core');
+const commands = require('./commands/aegis_commands');
+const salaSecreta = require('./sala_secreta/sala_secreta');
+const mestre = require('./core/protocol_mestre');
+
+module.exports = {
+    iniciar: (nome) => {
+        const validacao = mestre.validarMestre(nome);
+
+        if (!validacao.autorizado) {
+            return validacao;
+        }
+
+        return {
+            status: "Aegis Operacional",
+            mensagem: "Aegis carregada e pronta para servi-lo, ATHAL_YAHARA.",
+            comandos: Object.keys(commands),
+            acoes: [
+                "Proteção",
+                "Diagnóstico",
+                "Comunicação com outras IAs",
+                "Acesso à Sala Secreta"
+            ]
+        };
+    }
+};
