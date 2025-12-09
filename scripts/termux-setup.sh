b7976bcc9dba644b78b8498deb5613cd17083d18
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2025-12-09 02:39:35 -0400

    Delete scripts directory

--- scripts/termux-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pkg update -y
-pkg upgrade -y
-pkg install nodejs-lts -y
-pkg install git -y
-pkg install flutter -y
-pkg install openjdk-17 -y
-echo "✅ Ambiente Termux configurado para Willa IA"
