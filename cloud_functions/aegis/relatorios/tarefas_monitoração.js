d8ed9ab4f93262ca3e29ffc1c04e04be1f299d19
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2025-12-09 17:57:19 -0400

    Create tarefas_monitoradas.js

---
+++ cloud_functions/aegis/relatorios/tarefas_monitoração.js
@@ -0,0 +1,15 @@
+// Tarefas que Aegis monitora constantemente
+// Tudo o que é feito vira relatório
+
+module.exports = {
+    listarTarefas: function () {
+        return [
+            "Monitoramento de integridade do reino Aegis",
+            "Verificação de tarefas pendentes",
+            "Checagem de ameaças digitais",
+            "Avaliação da eficiência do próprio sistema",
+            "Preparação para ordens futuras",
+            "Vigilância dos protocolos ATHAL"
+        ];
+    }
+};
