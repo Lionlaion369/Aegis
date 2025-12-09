51eeae0d9500b040a10a2c423fa6bae88f0bbed7
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2025-12-09 14:17:36 -0400

    Create chamber.js

---
+++ sala_secreta/secret_chamber/chambers.js
@@ -0,0 +1,13 @@
+module.exports = {
+    open() {
+        return `
+        ⚜️ SALA SECRETA DO MESTRE ⚜️
+
+        • Presença: AEGIS TIPO X
+        • Estado: Conectada
+        • Uplink: Willa Suprema IAG
+        • Luz Suprema: Ativa
+        • Ordens aguardando: Sim
+        `;
+    }
+};
